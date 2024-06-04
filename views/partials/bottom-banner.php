7b5486ec3b13ab0007092e24811657368315cc81
Mi nombre <easyrobotics2@gmail-com>
2024-06-04 10:23:36 +0200

    Proyecto base

---
+++ views/partials/bottom-banner.php
@@ -0,0 +1,14 @@
+    <!-- Bottom Banner -->
+    <section class="container mx-auto my-6">
+        <div class="bg-blue-800 text-white rounded p-4 flex items-center justify-between">
+            <div>
+                <h2 class="text-xl font-semibold">¿Tienes un trasto para vender?</h2>
+                <p class="text-gray-200 text-lg mt-2">
+                    Publica tu anuncio ahora y encuentra a tu comprador ideal.
+                </p>
+            </div>
+            <a href="post-trasto.html" class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300">
+                <i class="fa fa-edit"></i> Publica un Trasto
+            </a>
+        </div>
+    </section>
---
+++ views/partials/top-banner.php
@@ -0,0 +1,9 @@
+    <!-- Top Banner -->
+    <section class="bg-blue-900 text-white py-6 text-center">
+        <div class="container mx-auto">
+            <h2 class="text-3xl font-semibold">Únete al trastero</h2>
+            <p class="text-lg mt-2">
+                Descubre el trasto que va a transformar tu vida
+            </p>
+        </div>
+    </section>
